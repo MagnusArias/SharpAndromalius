75ad7490538350aa425a518f217b9c6e8e1d05ba
Przemysław Dębiec <194933@edu.p.lodz.pl>
2017-10-09 15:21:54 +0200

    Added all classes from last project. TODO - make it compatible with MonoGame, not JavaSwing

---
+++ MainGame/MainGame/Control/Animation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp_Andromalius.GameData.Control
+{
+    class Animation
+    {
+        //TO-DO: zastąpić string odpowiednikiem BUfferedImage
+        private String[] frames;
+        private int currentFrame;
+        private int numFrames;
+
+        private int count;
+        private int delay;
+
+        private int timesPlayed;
+
+        public Animation()
+        {
+            timesPlayed = 0;
+        }
+
+        public void SetFrames(String[] frames)
+        {
+            this.frames = frames;
+            currentFrame = 0;
+            count = 0;
+            timesPlayed = 0;
+            delay = 5;
+            numFrames = frames.Length;
+        }
+
+        public void SetDelay(int i)
+        {
+            delay = i;
+        }
+
+        public void Update()
+        {
+            if (delay == -1) return;
+            count++;
+
+            if (count == delay)
+            {
+                currentFrame++;
+                count = 0;
+            }
+            if (currentFrame == numFrames)
+            {
+                currentFrame = 0;
+                timesPlayed++;
+            }
+        }
+
+        public String GetImage()
+        {
+            return frames[currentFrame];
+        }
+
+        public Boolean HasPlayesOnce()
+        {
+            return timesPlayed > 0;
+        }
+
+        public Boolean HasPlayed(int i)
+        {
+            return timesPlayed == i;
+        }
+    }
+}
---
+++ MainGame/MainGame/Maps/TileMap/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp_Andromalius.GameData.Maps.TileMap
+{
+    class Tile
+    {
+        // TODO - poszukac zamiennika buffered_image
+        private String image;
+        private int type;
+
+        //tile types
+        public const int AIR = 0;
+        public const int SOLID = 1;
+
+        public Tile(String image, int type)
+        {
+            this.image = image;
+            this.type = type;
+        }
+
+        public String getImage()
+        {
+            return image;
+        }
+
+        public int getType()
+        {
+            return type;
+        }
+    }
+}
